638b90505efcf3dc0f522c12c11971b31ab9747d
Marina <marina.zilkina@gmail.com>
2023-03-22 21:39:13 +0200

    backend part without swagger

--- Recipes/backend/models.py
+++ Recipes/backend/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+    image = models.FileField(upload_to='uploads/', blank=True, verbose_name='Изображение')
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey("Category", on_delete=models.CASCADE, null=True)
+    upload = models.FileField(upload_to='uploads/', blank=True, verbose_name='Изображение')
+
+    def __str__(self):
+        return self.title
+
