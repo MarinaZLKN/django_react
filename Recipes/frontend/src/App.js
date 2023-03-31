f9aa502d11cff1a975e94ee7b25a03285574d17e
Marina <marina.zilkina@gmail.com>
2023-03-31 19:00:33 +0300

    picture renders

--- Recipes/frontend/src/App.js
+++ Recipes/frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './App.css';
 import Header from "./components/Header/Header";
-import Home from'./components/Home'
+import Home from './components/Home'
 import RecipeList from "./components/RecipeList/RecipeList";
 import Recipe from "./components/Recipe/Recipe";
 import { Route, Routes, Link} from "react-router-dom";
--- Recipes/frontend/src/components/Home.jsx
+++ Recipes/frontend/src/components/Home.jsx
@@ -1,15 +1,14 @@
 import CategoryList from "./CategoryList";
 import React from "react";
-
+import pic from '../img/banner.jpg.webp';
+const picture = 'picture';
 function Home() {
-    const pic = '../../public/img/image.jpeg';
-    const picture = 'picture';
 
     return (
         <div className="content">
-            {/*<div className="pic">*/}
-            {/*    <img className="image" alt={picture} src={pic}/>*/}
-            {/*</div>*/}
+            <div className="pic">
+                <img className="image" alt={picture} src={pic}/>
+            </div>
             <CategoryList/>
         </div>
     )
